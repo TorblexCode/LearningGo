30c1e26e796027b60bdf581e7f9ec23a62f241b3
Leandro Texidor Basterrechea <leandro.texidor@gmail.com>
2019-11-15 02:00:21 -0400

    Clase 2, declaracion de variables

---
+++ clase_02/Variables y Constantes /main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// TODO Variables y constates en Go
+func main() {
+	/* TODO Declaracion de variables en Go
+	   (palabra reservada) => var (name) (tipo_dato)
+	*/
+	// var name, lastname string
+	name, lastname := "Leandro", "Texidor"
+
+	fmt.Println(name, lastname)
+}
